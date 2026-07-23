d77f43b896a2a029d711917bc702cc509b90b884
deepakvssdr <deepakvssdr@gmail.com>
2026-07-23 22:18:47 +0530

    Time: 0 ms (100%), Space: 59.9 MB (86.67%) - LeetHub

---
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAbsoluteSum = function(nums) {
+    let best1=nums[0],res=nums[0],best2=nums[0];
+    for(let i=1;i<nums.length;i++){
+        best1=Math.max(best1+nums[i],nums[i]);
+        best2=Math.min(best2+nums[i],nums[i]);
+        res=Math.max(res,Math.abs(best2),best1);
+    }
+    return Math.abs(res);
+};
