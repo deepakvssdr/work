1ca6aba9dc9d0abc8d48f8053113eb1476198cab
deepakvssdr <deepakvssdr@gmail.com>
2026-07-23 16:59:54 +0530

    Time: 1 ms (87.36%), Space: 64 MB (48.65%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -6,10 +6,8 @@ var maxSubArray = function(nums) {
     let best=nums[0];
     let ans=nums[0];
     let v1=0,v2=0;
-    for(let i=1;i<nums.length;i++){
-        v1=best+nums[i];
-        v2=nums[i];
-        best=Math.max(v1,v2);
+    for(let i=1;i<nums.length;i++){       
+        best=Math.max((best+nums[i]),nums[i]);
         ans=Math.max(best,ans);
     }
     return ans;
