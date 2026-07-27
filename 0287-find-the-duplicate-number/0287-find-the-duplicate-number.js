cf7834cad83d41c488d7dbd5ad507e6fcdf036ad
deepakvssdr <deepakvssdr@gmail.com>
2026-07-27 19:26:12 +0530

    Time: 6 ms (87.92%), Space: 63.9 MB (52.41%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    let slow=0,fast=0;
+    while(true){
+        slow=nums[slow];
+        fast=nums[nums[fast]];
+        if(slow==fast){
+            slow=0;
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+     }
+    }
+     return false; 
+};
