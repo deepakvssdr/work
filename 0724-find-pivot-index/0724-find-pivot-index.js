ee22c20554aa0e92434122a7c064f02eeea294b0
deepakvssdr <deepakvssdr@gmail.com>
2026-07-30 23:33:56 +0530

    Time: 1 ms (90.63%), Space: 56.2 MB (89.64%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let left=0,right=0,sum=0;
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+    }
+    for(let i=0;i<nums.length;i++){
+        right=sum-nums[i]-left;
+        if(left==right){
+            return i;
+        }
+        left+=nums[i];
+    }
+    return -1;
+};
