6f44f8d9ad2ae3daf78ae4a63f97a29702b490e6
deepakvssdr <deepakvssdr@gmail.com>
2026-08-01 19:30:15 +0530

    Time: 14 ms (84.68%), Space: 61.3 MB (32.73%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.js
@@ -4,15 +4,12 @@
  * @return {number}
  */
 var subarraysDivByK = function(nums, k) {
-    let sum=0,res=0,rem,freq;
+    let sum=0,res=0,freq,rem;
     let map=new Map([[0,1]]);
     for(let i=0;i<nums.length;i++){
         sum+=nums[i];
         rem=sum%k;
-        if(rem<0){
-            rem+=k;
-        }
-        //freq=map.get(rem)||0;
+        if(rem<0) rem+=k;
         res+=(map.get(rem)||0);
         map.set(rem,(map.get(rem)||0)+1);
     }
