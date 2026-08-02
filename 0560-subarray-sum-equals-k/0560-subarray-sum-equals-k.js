d774e7bd4d8caf6fdf4d4fa4c4213a3c52622f73
deepakvssdr <deepakvssdr@gmail.com>
2026-08-02 11:49:57 +0530

    Time: 12 ms (94.4%), Space: 61.7 MB (76.55%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -4,15 +4,14 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    let map=new Map();
-    map.set(0,1);
-    sum=0,res=0;
+    let map=new Map([[0,1]]);
+    let sum=0,res=0,rem=0;
     for(let i=0;i<nums.length;i++){
-        sum=sum+nums[i];
-        let x=sum-k;
-        let freq=map.get(x)||0;
-        res=res+freq;
+        sum+=nums[i];
+        rem=sum-k;
+        freq=map.get(rem)||0;
+        res+=freq;
         map.set(sum,(map.get(sum)||0)+1);
     }
     return res;
-};
+    };
