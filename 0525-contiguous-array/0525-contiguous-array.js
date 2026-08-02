b1046e45e141e86b13a46fd7cb69afa35b8b6d03
deepakvssdr <deepakvssdr@gmail.com>
2026-08-02 19:26:50 +0530

    Time: 15 ms (54.08%), Space: 63.4 MB (31.01%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.js
+++ 0525-contiguous-array/0525-contiguous-array.js
@@ -8,15 +8,16 @@ var findMaxLength = function(nums) {
     for(let i=0;i<nums.length;i++){
         if(nums[i]==0) zero++;
         else one++;
-        diff=zero-one;
-        if(diff==0){
+        //diff=zero-one;
+        if(zero-one==0){
             res=Math.max(res,i+1);
+            continue;
         }
-        if(!map.has(diff)){
-            map.set(diff,i);
+        if(!map.has(zero-one)){
+            map.set(zero-one,i);
         }
         else{
-            let index=map.get(diff);
+            let index=map.get(zero-one);
             let len=i-index;
             res=Math.max(res,len);
         }
