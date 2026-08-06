3a1d708dbf97455eb333e0ef0c428dcb99d083bc
deepakvssdr <deepakvssdr@gmail.com>
2026-08-06 10:24:46 +0530

    Time: 9 ms (51.49%), Space: 66.8 MB (18.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -4,10 +4,10 @@
  */
 var merge = function(intervals) {
     intervals.sort((a,b)=>a[0]-b[0]);
-    let s1=intervals[0][0];
-    let e1=intervals[0][1];
-    let s2,e2;
     let res=[];
+    let s1,e1,s2,e2;
+    s1=intervals[0][0];
+    e1=intervals[0][1];
     for(let i=1;i<intervals.length;i++){
         s2=intervals[i][0];
         e2=intervals[i][1];
