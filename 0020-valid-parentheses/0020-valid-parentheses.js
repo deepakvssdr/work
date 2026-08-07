783a3f16702cf66b0882b31e19b27f4a73752475
deepakvssdr <deepakvssdr@gmail.com>
2026-08-07 19:10:53 +0530

    Time: 0 ms (100%), Space: 56.1 MB (43.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -6,9 +6,11 @@ var isValid = function(s) {
     let stack=[];
     let top=-1;
     for(let i=0;i<s.length;i++){
-        if(stack[top]=="("&&s[i]==")"||
+        if(
+        stack[top]=="("&&s[i]==")"||
         stack[top]=="["&&s[i]=="]"||
-        stack[top]=="{"&&s[i]=="}"){
+        stack[top]=="{"&&s[i]=="}"
+        ){
             stack.pop();
             top--;
             continue;
