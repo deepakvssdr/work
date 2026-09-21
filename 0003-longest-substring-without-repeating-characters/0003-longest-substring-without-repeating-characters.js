ec856ccc86d0d48ab3d6e2eb31072866cae949f5
deepakvssdr <deepakvssdr@gmail.com>
2026-09-21 18:57:07 +0530

    Time: 114 ms (14.28%), Space: 60.6 MB (67.87%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let left=0,right=0,len=0;
-    let map=new Map();
-    for(right=0;right<s.length;right++){
-        map.set(s[right],(map.get(s[right])||0)+1);
-        while(map.get(s[right])>1){
-            map.set(s[left],map.get(s[left])-1);
-            if(map.get(s[left])==0){
-                map.delete(s[left]);
-            }
-            left++;
+   let left=0,right=0,len=0;
+   let map=new Map();
+   for(right=0;right<s.length;right++){
+    map.set(s[right],(map.get(s[right])||0)+1);
+    while(map.get(s[right])>1){
+        map.set(s[left],map.get(s[left])-1);
+        if(map.get(s[left])==0){
+            map.delete(s[left]);
         }
-        len=Math.max(len,right-left+1);
+        left++;
     }
-    return len;
+    len=Math.max(len,right-left+1);
+   } 
+   return len;
 };
